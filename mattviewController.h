da9dc06fdb9f58d96b5841061fedc3e369026b50
xusea <xusea@xuseadeMacBook-Pro.local>
2016-08-25 18:06:57 +0800

    mattviewcontroller

---
+++ mattviewController.h
@@ -0,0 +1,13 @@
+//
+//  mattviewController.h
+//  testDrawSketch
+//
+//  Created by xusea on 16/8/25.
+//  Copyright © 2016年 xusea. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface mattviewController : NSViewController
+
+@end
---
+++ mattviewController.m
@@ -0,0 +1,22 @@
+//
+//  mattviewController.m
+//  testDrawSketch
+//
+//  Created by xusea on 16/8/25.
+//  Copyright © 2016年 xusea. All rights reserved.
+//
+
+#import "mattviewController.h"
+
+@interface mattviewController ()
+
+@end
+
+@implementation mattviewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
