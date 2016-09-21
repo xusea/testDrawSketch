f67e9bfbd601bab4f9af5e834eff6107fdaf7d56
xusea <xusea@xuseadeMacBook-Pro.local>
2016-09-21 16:29:33 +0800

    add resultdetailViewController

---
+++ testDrawSketch/resultdetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  resultdetailViewController.h
+//  testDrawSketch
+//
+//  Created by xusea on 2016/9/21.
+//  Copyright © 2016年 xusea. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface resultdetailViewController : NSViewController
+
+@end
---
+++ testDrawSketch/resultdetailViewController.m
@@ -0,0 +1,22 @@
+//
+//  resultdetailViewController.m
+//  testDrawSketch
+//
+//  Created by xusea on 2016/9/21.
+//  Copyright © 2016年 xusea. All rights reserved.
+//
+
+#import "resultdetailViewController.h"
+
+@interface resultdetailViewController ()
+
+@end
+
+@implementation resultdetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
