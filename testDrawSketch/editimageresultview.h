7dbd4b6af231b9aeb60d909125789cc1ef7544f9
xusea <xusea@xuseadeMacBook-Pro.local>
2016-11-04 00:56:18 +0800

    fix bug

--- testDrawSketch/editimageresultview.h
+++ testDrawSketch/editimageresultview.h
@@ -16,12 +16,21 @@
     int dragflag;
     NSPoint lastpoint;
     query2image * q2i;
+    int status;//0 正常 1 选中 2 左上 3 坐下 4 右上 5 右下
+    int resizegap;
+    float zoomfactor;
+    NSSize cornersize;
 }
 @property resultimageview * riv;
 @property NSRect selectedrect;
 @property int dragflag;
 @property NSPoint lastpoint;
 @property query2image * q2i;
+@property int status;
+@property int resizegap;
+@property float zoomfactor;
+@property NSSize cornersize;
 -(void)initial;
--(void)getSelectedDS:(NSPoint)point;
+-(int)getSelectedDS:(NSPoint)point;
+-(int)checkclickcorner:(NSPoint)point;
 @end
--- testDrawSketch/editimageresultview.m
+++ testDrawSketch/editimageresultview.m
@@ -14,11 +14,19 @@
 @synthesize dragflag;
 @synthesize lastpoint;
 @synthesize q2i;
+@synthesize status;
+@synthesize resizegap;
+@synthesize zoomfactor;
+@synthesize cornersize;
 -(void)initial
 {
     selectedrect = NSZeroRect;
     dragflag = 0;
     q2i = nil;
+    status = 0;
+    resizegap = 40;
+    zoomfactor = 1.0;
+    cornersize = NSMakeSize(20, 20);
 }
 - (void)drawRect:(NSRect)dirtyRect {
     [super drawRect:dirtyRect];
@@ -39,11 +47,73 @@
 - (void)mouseDown:(NSEvent *)theEvent
 {
     NSPoint currentPosition = [self convertPoint:[theEvent locationInWindow] fromView:nil];
-    [self getSelectedDS:currentPosition];
+    int newstatus = [self getSelectedDS:currentPosition];
+    if(newstatus == 0)
+    {
+        status = 0;
+        [self setNeedsDisplay:YES];
+        return;
+    }
+    if(status == 0)
+    {
+        status = 1;
+        [self setNeedsDisplay:YES];
+        return;
+    }
+    
+    NSRect lefttop, righttop, leftbuttom, rightbuttom;
+    NSSize cur_cornersize = cornersize;
+    cur_cornersize.width /= zoomfactor;
+    cur_cornersize.height /= zoomfactor;
+    lefttop.size = cur_cornersize;
+    righttop.size = cur_cornersize;
+    leftbuttom.size = cur_cornersize;
+    rightbuttom.size = cur_cornersize;
+    
+    lefttop.origin.x = selectedrect.origin.x;
+    lefttop.origin.y = selectedrect.origin.y + selectedrect.size.height - cur_cornersize.height;
+    
+    righttop.origin.x = selectedrect.origin.x + selectedrect.size.width - cur_cornersize.width;
+    righttop.origin.y = selectedrect.origin.y + selectedrect.size.height - cur_cornersize.height;
+    
+    leftbuttom.origin.x = selectedrect.origin.x;
+    leftbuttom.origin.y = selectedrect.origin.y;
+    
+    rightbuttom.origin.x = selectedrect.origin.x + selectedrect.size.width - cur_cornersize.width;
+    rightbuttom.origin.y = selectedrect.origin.y;
+    
+    if(NSPointInRect(currentPosition, lefttop))
+    {
+        NSLog(@"click in lefttop");
+        status = 2;
+        return;
+    }
+    
+    if(NSPointInRect(currentPosition, leftbuttom))
+    {
+        NSLog(@"click in leftbuttom");
+        status = 3;
+        return;
+    }
+    
+    if(NSPointInRect(currentPosition, righttop))
+    {
+        NSLog(@"click in righttop");
+        status = 4;
+        return;
+    }
+    
+    if(NSPointInRect(currentPosition, rightbuttom))
+    {
+        NSLog(@"click in rightbuttom");
+        status = 5;
+        return;
+    }
+
+    
     dragflag = 1;
     lastpoint = currentPosition;
-    //NSRect locat;
-    //locat.origin = currentPosition;
+    [self setNeedsDisplay:YES];
 }
 
 - (void)mouseDragged:(NSEvent *)theEvent
@@ -54,32 +124,87 @@
     {
         if(!NSEqualRects(selectedrect, NSZeroRect))
         {
-            selectedrect.origin.x -=  lastpoint.x - currentPosition.x;
-            selectedrect.origin.y -=  lastpoint.y - currentPosition.y;
-            [q2i setImagedrawrect:selectedrect];
-            [riv setNeedsDisplay:YES];
-            //[q2i imagedrawrect].origin.x -=  lastpoint.x - currentPosition.x;
-           //
+            if(status == 1)
+            {
+                selectedrect.origin.x -=  lastpoint.x - currentPosition.x;
+                selectedrect.origin.y -=  lastpoint.y - currentPosition.y;
+            }
+            else
+            {
+                //lefttop
+                if(status == 2
+                   && currentPosition.x + resizegap < selectedrect.origin.x + selectedrect.size.width
+                   && currentPosition.y > selectedrect.origin.y + resizegap)
+                {
+                    selectedrect.origin.x += currentPosition.x - lastpoint.x;
+                    //selectedrect.origin.y += currentPosition.y - lastpoint.y;
+                    selectedrect.size.height += currentPosition.y - lastpoint.y;
+                    selectedrect.size.width -= currentPosition.x - lastpoint.x;
+                    lastpoint = currentPosition;
+                    
+                }
+                //leftbuttom
+                if(status == 3
+                   && currentPosition.x + resizegap < selectedrect.origin.x + selectedrect.size.width
+                   && currentPosition.y + resizegap < selectedrect.origin.y + selectedrect.size.height)
+                {
+                    selectedrect.origin.x += currentPosition.x - lastpoint.x;
+                    selectedrect.origin.y += currentPosition.y - lastpoint.y;
+                    selectedrect.size.width -= currentPosition.x - lastpoint.x;
+                    selectedrect.size.height -= currentPosition.y - lastpoint.y;
+                    lastpoint = currentPosition;
+                    
+                }
+                //righttop
+                if(status == 4
+                   && currentPosition.x - resizegap > selectedrect.origin.x
+                   && currentPosition.y - resizegap > selectedrect.origin.y)
+                {
+                    selectedrect.size.height += currentPosition.y - lastpoint.y;
+                    selectedrect.size.width += currentPosition.x - lastpoint.x;
+                    lastpoint = currentPosition;
+                    
+                }
+                //rightbuttom
+                if(status == 5
+                   && currentPosition.x - resizegap > selectedrect.origin.x
+                   && currentPosition.y + resizegap < selectedrect.origin.y + selectedrect.size.height)
+                {
+                    selectedrect.origin.y += currentPosition.y - lastpoint.y;
+                    selectedrect.size.height -= currentPosition.y - lastpoint.y;
+                    selectedrect.size.width += currentPosition.x - lastpoint.x;
+                    lastpoint = currentPosition;
+                    
+                }
+            }
             
-            [self setNeedsDisplay:YES];
+            
         }
+        [q2i setImagedrawrect:selectedrect];
+        [riv setNeedsDisplay:YES];
+        [self setNeedsDisplay:YES];
     }
+    
     lastpoint = currentPosition;
     dragflag = 1;
 }
 
 - (void)mouseUp:(NSEvent *)theEvent
 {
-    selectedrect = NSZeroRect;
+    //selectedrect = NSZeroRect;
     dragflag = 0;
-    q2i = nil;
-    [self setNeedsDisplay:YES];
+    if(status == 2 || status == 3 || status == 4 || status == 5)
+    {
+        status = 1;
+    }
+    //q2i = nil;
+    //[self setNeedsDisplay:YES];
 }
--(void)getSelectedDS:(NSPoint)point
+-(int)getSelectedDS:(NSPoint)point
 {
     if(riv == nil)
     {
-        return;
+        return 0;
     }
     for(int i = 0; i < [[riv querydrawlist] count] ; i++)
     {
@@ -88,8 +213,20 @@
         {
             selectedrect = [q2i imagedrawrect];
             [self setNeedsDisplay:YES];
-            break;
+            return 1;
         }
     }
+    selectedrect = NSZeroRect;
+    q2i = nil;
+    return 0;
+}
+-(int)checkclickcorner:(NSPoint)point
+{
+    if(NSEqualRects(selectedrect, NSZeroRect))
+    {
+        return 0;
+    }
+    
+    return 0;
 }
 @end
